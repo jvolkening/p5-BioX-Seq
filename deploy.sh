4e3cd555ca50e15d0efa5ef119591185866f6f30
jvolkening <jdv@base2bio.com>
2018-01-12 15:00:58 -0600

    add debug info to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env sh
 
+echo "Checking for existing build files..."
 if [ -f "Build" ]
 then
     ./Build distclean
 fi
+echo "Preparing build..."
 perl Build.PL
+echo "Building and deploying..."
 ./Build dist \
   | grep -oP 'Creating \K.+\.tar\.gz' \
   | xargs cpan-upload -u $PAUSE_USER -p $PAUSE_PASS --dry-run
