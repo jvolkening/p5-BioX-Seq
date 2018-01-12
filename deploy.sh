a2fe7883183a083bf3fe2022f31f42caec461919
jvolkening <jdv@base2bio.com>
2018-01-12 15:20:12 -0600

    yet another try at script deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Checking for existing build files..."
 if [ -f "Build" ]
 then
     ./Build distclean
 fi
+
 echo "Preparing build..."
 perl Build.PL
-echo "Building and deploying..."
+
+echo "Building and deploying to CPAN..."
 ./Build dist \
   | grep -oP 'Creating \K.+\.tar\.gz' \
   | xargs cpan-upload -u $PAUSE_USER -p $PAUSE_PASS --dry-run
